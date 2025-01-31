defbb32bd513c280737acbcb793efb4e2cb5446a
Marjina2 <162699004+Marjina2@users.noreply.github.com>
2025-01-31 22:25:11 +0530

    14 commit

--- next.config.js
+++ next.config.js
@@ -7,11 +7,15 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_SITE_URL: process.env.URL || process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
   },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
+      punycode: false,
     };
     return config;
   }
