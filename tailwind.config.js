99412516eb2b54d40c0f2a36639a66093ae5f2bd
Marjina2 <162699004+Marjina2@users.noreply.github.com>
2025-01-31 21:56:12 +0530

    9 commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,23 +5,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      keyframes: {
-        modalSlide: {
-          '0%': { 
-            opacity: '0',
-            transform: 'translateY(-16px)'
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)'
-          }
-        }
-      },
-      animation: {
-        modalSlide: 'modalSlide 0.2s ease-out forwards'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 } 
