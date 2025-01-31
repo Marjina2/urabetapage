d68186118cb68ac6c321200d2c963179639e5aa2
Marjina2 <162699004+Marjina2@users.noreply.github.com>
2025-01-31 23:05:20 +0530

    20 commit

--- next.config.js
+++ next.config.js
@@ -1,23 +1,40 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `buffer` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        punycode: false,
+        buffer: false,
+        stream: false,
+        util: false,
+      };
+
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+          process: 'process/browser',
+        })
+      );
+    }
+    return config;
+  },
   env: {
     NEXT_PUBLIC_SITE_URL: process.env.URL || process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      punycode: false,
-    };
-    return config;
   }
 }
 
